8967880a4cb5ebf70f10ef34bffa4c0dfff71fbe
raynertjx <rayner742@gmail.com>
2022-12-11 01:20:08 +0800

    Added forum skeleton

--- src/routes/Forum.tsx
+++ src/routes/Forum.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Forum: React.FC = () => {
-    return <div>Forum</div>
-}
+    return (
+        <div className="w-full">
+            <div className="text-center">
+                <h1 className="text-7xl pb-8">The Soccat Forum</h1>
+                <p className="text-2xl">Explore the forum below!</p>
+            </div>
+            <div className="bg-white">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </div>
+        </div>
+    );
+};
 
 export default Forum;
