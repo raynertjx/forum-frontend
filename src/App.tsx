5b0e04be353b3f72ff6f19527dcf5dd7f44975b9
raynertjx <rayner742@gmail.com>
2022-12-11 01:16:59 +0800

    Adjusted routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import About from "./pages/About";
-import Forum from "./pages/Forum";
-import Login from "./pages/Login";
+import About from "./routes/About";
+import Forum from "./routes/Forum";
+import Login from "./routes/Login";
 
 function App() {
     return (
-        <Layout>
-            <Routes>
-                <Route path="/" element={<Forum />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<Navigate to="/" replace />}/>
-            </Routes>
-        </Layout>
+          <Layout>
+              <Routes>
+                  <Route path="/" element={<Forum />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="*" element={<Navigate to="/" replace />}/>
+              </Routes>
+          </Layout>
     );
 }
 export default App;
