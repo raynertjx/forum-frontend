77b8b316b26daeb680be14b139ce50697a16e4a0
raynertjx <rayner742@gmail.com>
2022-12-11 01:19:53 +0800

    Adjusted layout styles

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
 interface Props {
     children: React.ReactNode;
-  }
+}
 
 const Layout: React.FC<Props> = (props) => {
     return (
         <>
             <Header />
-            <main>{props.children}</main>
+            <main className="mx-auto w-full max-w-5xl">
+                {props.children}
+            </main>
         </>
-    )
-}
+    );
+};
 
 export default Layout;
