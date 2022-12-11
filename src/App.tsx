74557bb11497fcce5384358d8e5c38e30fda0977
raynertjx <rayner742@gmail.com>
2022-12-12 01:27:05 +0800

    Added subforum routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import Layout from "./components/layout/Layout";
 import About from "./routes/About";
 import Forum from "./routes/Forum";
 import Login from "./routes/Login";
+import Subforum from "./routes/Subforum";
 
 function App() {
     return (
-          <Layout>
-              <Routes>
-                  <Route path="/" element={<Forum />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="*" element={<Navigate to="/" replace />}/>
-              </Routes>
-          </Layout>
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Navigate to="/forum" replace />} />
+                <Route path="/forum" element={<Forum />} />
+                <Route path="/forum/:forumId" element={<Subforum/>}/>
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </Layout>
     );
 }
 export default App;
