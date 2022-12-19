23ba5e95d1cbdb2319d540ba41c26b411fb66b1b
raynertjx <rayner742@gmail.com>
2022-12-20 00:48:26 +0800

    replaced with ui taskbar component

--- src/components/forum/ForumTaskBar.tsx
+++ src/components/forum/ForumTaskBar.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
+import Taskbar from "../UI/Taskbar";
 
 const ForumTaskBar: React.FC = () => {
-    return <div className="grid w-full bg-blue-400 mt-16 py-3 px-4 grid-cols-10 font-medium text-lg">
-        <div className="col-span-6">Categories</div>
-        <div className="hidden lg:block lg:col-span-2">Latest Thread</div>
-        <div className="hidden lg:block lg:col-span-1">Threads</div>
-        <div className="hidden lg:block lg:col-span-1">Comments</div>
-    </div>;
+    return <Taskbar headers={["Categories", "Latest Thread", "Threads", "Comments"]}/>
 };
 
 export default ForumTaskBar;
