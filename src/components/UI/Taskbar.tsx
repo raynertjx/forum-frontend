1e9076012278995e9faef51516c1705793c1dde4
raynertjx <rayner742@gmail.com>
2022-12-20 00:27:36 +0800

    initialised taskbar ui component

---
+++ src/components/UI/Taskbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Taskbar: React.FC<{headers: string[]}> = ({ headers }) => {
+    return <div className="grid w-full bg-blue-400 mt-16 py-3 px-4 grid-cols-10 font-medium text-lg">
+        <div className="col-span-6">{headers[0]}</div>
+        <div className="hidden lg:block lg:col-span-2">{headers[1]}</div>
+        <div className="hidden lg:block lg:col-span-1">{headers[2]}</div>
+        <div className="hidden lg:block lg:col-span-1">{headers[3]}</div>
+    </div>;
+};
+
+export default Taskbar;
