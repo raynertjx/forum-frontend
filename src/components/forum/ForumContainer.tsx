2626dbb7ed500b275a2429a6c2f5f7791e0b9ae6
raynertjx <rayner742@gmail.com>
2022-12-20 00:47:58 +0800

    replaced with ui container component

--- src/components/forum/ForumContainer.tsx
+++ src/components/forum/ForumContainer.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import ForumTaskBar from "./ForumTaskBar";
 import ForumCategories from "./ForumCategories";
+import Container from "../UI/Container";
 
 const ForumContainer: React.FC = () => {
     return (
-        <div className="mx-auto w-10/12 lg:w-full bg-gray-100 shadow-xl mt-16 rounded-xl">
-            <ForumTaskBar />
-            <ForumCategories />
-        </div>
+            <Container>
+                <ForumTaskBar />
+                <ForumCategories />
+            </Container>
     );
 };
 
