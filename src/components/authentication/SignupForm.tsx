6a53242bb3316f2266dd1ee2179852d9d1e2d7c0
raynertjx <rayner742@gmail.com>
2022-12-23 16:00:21 +0800

    added signup form

--- src/components/authentication/SignupForm.tsx
+++ src/components/authentication/SignupForm.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import Services from "../../services/Services";
+import { useAppDispatch } from "../../hooks/hooks";
+import { authActions } from "../../store/auth-slice";
+import { useNavigate } from "react-router-dom";
 
 const SignupForm: React.FC = () => {
-    return <div>signupform</div>
-}
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const usernameInput = useRef<HTMLInputElement | null>(null);
+    const passwordInput = useRef<HTMLInputElement | null>(null);
+    const confirmPasswordInput = useRef<HTMLInputElement | null>(null);
+    const formSubmitHandler = async (event: React.FormEvent) => {
+        event.preventDefault();
+        await Services.signup({
+            username: usernameInput.current?.value,
+            password: passwordInput.current?.value,
+            password_confirmation: confirmPasswordInput.current?.value,
+        })
+            .then((res) => {
+                console.log(res);
+                const data = res.data.user;
+                // dispatch(
+                //     authActions.signup({
+                //         user_id: data.id,
+                //         username: data.username,
+                //     })
+                // );
+                navigate("/login");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <label htmlFor="username">Username</label>
+            <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+                ref={usernameInput}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                ref={passwordInput}
+            />
+            <label htmlFor="password_confirmation">Confirm Password</label>
+            <input
+                type="password"
+                name="password_confirmation"
+                id="password_confirmation"
+                placeholder="Confirm Password"
+                ref={confirmPasswordInput}
+            />
+            <input type="submit" value="Submit" />
+        </form>
+    );
+};
 
 export default SignupForm;
