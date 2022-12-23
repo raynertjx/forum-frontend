13bb1324e63ecf08d6cafeb7c734f380dd1785b1
raynertjx <rayner742@gmail.com>
2022-12-23 23:09:02 +0800

    fixed useEffect bug where threads were duplicated

--- src/routes/Subforum.tsx
+++ src/routes/Subforum.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch } from "../helpers/hooks";
+import { threadActions } from "../store/thread-slice";
 import Title from "../components/UI/Title";
+import SubforumContainer from "../components/subforum/SubforumContainer";
 import { FORUM_CATEGORIES } from "../components/forum/Forum.constants";
 import { useParams } from "react-router-dom";
+import Services from "../services/Services";
 
 const Subforum: React.FC = () => {
     const { forumId } = useParams() as { forumId: string };
     const temp = FORUM_CATEGORIES[forumId];
-    return <Title title={temp.title} desc={temp.subtitle} />;
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(threadActions.removeAllThreads());
+        const fetchThreads = async () => {
+            await Services.get_threads().then((res) => {
+                dispatch(threadActions.getAllThreads(res.data));
+            });
+        };
+        fetchThreads();
+    }, []);
+
+    return (
+        <>
+            <Title title={temp.title} desc={temp.subtitle} />
+            <SubforumContainer />
+        </>
+    );
 };
 
 export default Subforum;
