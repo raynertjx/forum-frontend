d6daf8a04450642b113bc9e293c6086ba8ffa2be
raynertjx <rayner742@gmail.com>
2022-12-23 22:54:18 +0800

    added forum thread reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth-slice";
+import threadSlice from "./thread-slice";
 
 export const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
+        threads: threadSlice.reducer,
     },
 });
 
