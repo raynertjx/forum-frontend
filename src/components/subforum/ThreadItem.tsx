fc3e09655873b89fb3487347a5f2cc58a981e35d
raynertjx <rayner742@gmail.com>
2022-12-23 22:58:09 +0800

    modified to include author and created_at date

---
+++ src/components/subforum/ThreadItem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { ThreadType } from "./Thread.types";
+import { formatDate } from "../../helpers/helpers";
+
+const ThreadItem: React.FC<{ url: string } & ThreadType> = (props) => {
+    return (
+        <div className="grid lg:grid-cols-10 px-4 py-4">
+            <div className="col-span-6 grid grid-cols-3 lg:flex gap-4">
+                <div></div>
+                <div className="col-span-2">
+                    <NavLink
+                        className="font-semibold text-lg"
+                        to={`/forum/${props.url}`}
+                    >
+                        {props.title}
+                    </NavLink>
+                    <div className="flex gap-4">
+                        <p>by {props.author}</p>
+                        <p>{formatDate(props.created_at)}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden lg:block col-span-2">100</div>
+            <div className="hidden lg:block col-span-1">100</div>
+            <div className="hidden lg:block col-span-1">100</div>
+        </div>
+    );
+};
+
+export default ThreadItem;
