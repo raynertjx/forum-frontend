7ad1f477bc1d23843a3225905556899d7cf41a10
raynertjx <rayner742@gmail.com>
2022-12-24 18:35:36 +0800

    adjusted margin

--- src/components/UI/Taskbar.tsx
+++ src/components/UI/Taskbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Taskbar: React.FC<{headers: string[]}> = ({ headers }) => {
-    return <div className="grid w-full bg-blue-400 mt-16 py-3 px-4 grid-cols-10 font-medium text-lg">
+    return <div className="grid w-full bg-blue-400 py-3 px-4 grid-cols-10 font-medium text-lg">
         <div className="col-span-6">{headers[0]}</div>
         <div className="hidden lg:block lg:col-span-2">{headers[1]}</div>
         <div className="hidden lg:block lg:col-span-1">{headers[2]}</div>
