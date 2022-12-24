24b8092ecc1c01713f41a4f15b500807cd5673ed
raynertjx <rayner742@gmail.com>
2022-12-24 18:35:25 +0800

    removed box shadow

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Container: React.FC<Props> = (props) => {
     return (
-        <div className="mx-auto w-10/12 lg:w-full bg-gray-100 shadow-xl mt-16 rounded-xl">
+        <div className="mx-auto w-10/12 lg:w-full bg-gray-100 mt-16 rounded-xl">
             {props.children}
         </div>
     );
