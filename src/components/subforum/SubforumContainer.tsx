eb2f1073d943632095b6447833db9b41036f524f
raynertjx <rayner742@gmail.com>
2022-12-25 22:11:49 +0800

    modified ThreadItem props and added isLoggedIn variable

--- src/components/subforum/SubforumContainer.tsx
+++ src/components/subforum/SubforumContainer.tsx
@@ -2,32 +2,42 @@ import React from "react";
 import Container from "../UI/Container";
 import Taskbar from "../UI/Taskbar";
 import ThreadItem from "./ThreadItem";
-import Button from "../UI/Button";
 import { formatUrl } from "../../helpers/helpers";
 import { useAppSelector } from "../../helpers/hooks";
 import { NavLink } from "react-router-dom";
 
-const SubforumContainer: React.FC = () => {
-    const allThreads = useAppSelector((state) => state.threads);
+type Prop = { category: string };
+
+const SubforumContainer: React.FC<Prop> = (props: Prop) => {
+    const allThreads = useAppSelector((state) =>
+        state.threads.filter((thread) => thread.category === props.category)
+    );
+    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
 
     return (
         <Container>
             <div className="bg-white flex justify-end pb-3">
-                <NavLink to="new" className="bg-blue-400 px-3 py-1">
+                {isLoggedIn && <NavLink
+                    to="new"
+                    className="bg-blue-400 px-3 py-1"
+                    state={{ category: props.category }}
+                >
                     New Thread
-                </NavLink>
+                </NavLink>}
             </div>
             <Taskbar
                 headers={["Thread", "Latest Comment", "Comments", "Views"]}
             />
             <div className="flex flex-col">
-                {allThreads.map((item) => (
+                {allThreads.map((thread) => (
                     <ThreadItem
-                        url={formatUrl(item.id, item.title)}
-                        title={item.title}
-                        author={item.username}
-                        category={item.category}
-                        created_at={item.created_at}
+                        id={thread.id}
+                        url={formatUrl(thread.id, thread.title)}
+                        title={thread.title}
+                        content={thread.content}
+                        author={thread.username}
+                        category={thread.category}
+                        created_at={thread.created_at}
                     />
                 ))}
             </div>
