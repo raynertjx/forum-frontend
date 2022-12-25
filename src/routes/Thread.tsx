9ee7aa4c12fbe73c22627558f7c79f2d648263c2
raynertjx <rayner742@gmail.com>
2022-12-25 17:38:23 +0800

    initialised Thread route

--- src/routes/Thread.tsx
+++ src/routes/Thread.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useAppSelector } from "../helpers/hooks";
+import { useParams } from "react-router-dom";
 
 const Thread: React.FC = () => {
-    return <div>hi</div>
-}
+    const { threadId } = useParams() as { threadId: string };
+    const currentThreadId = Number(
+        threadId.substring(0, threadId.indexOf("-"))
+    );
+    const currentThread = useAppSelector((state) => state.threads).find(
+        (item) => item.id === currentThreadId
+    );
+
+    return <div>{currentThread?.content}</div>;
+};
 
 export default Thread;
