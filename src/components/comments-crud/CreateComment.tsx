1c581f2e2e10826241691702ed34f79faca87807
raynertjx <rayner742@gmail.com>
2023-01-12 14:06:49 +0800

    initialised CreateComment

---
+++ src/components/comments-crud/CreateComment.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { commentServices } from "../../services/Services";
+import { useAppDispatch } from "../../helpers/hooks";
+import { threadActions } from "../../store/thread-slice";
+import CommentForm from "./CommentForm";
+
+type Prop = {
+    threadId: number;
+};
+
+const CreateComment: React.FC<Prop> = (props: Prop) => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const contentInput = useRef<HTMLInputElement | null>(null);
+
+    const createCommentHandler = async (event: React.FormEvent) => {
+        event.preventDefault(); 
+        await commentServices
+            .create_comment({
+                content: contentInput.current?.value,
+                forum_thread_id: props.threadId,
+            })
+            .then((res) => {
+                console.log(res.data.id);
+                dispatch(threadActions.getAllThreads);
+                navigate(0);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    return (
+        <CommentForm
+            formSubmitHandler={createCommentHandler}
+            contentRef={contentInput}
+            contentValue={""}
+        />
+    );
+};
+
+export default CreateComment;
