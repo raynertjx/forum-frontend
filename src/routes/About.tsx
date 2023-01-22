44cf2b2683b6227b68e75502a5411a57f411d4b2
raynertjx <rayner742@gmail.com>
2023-01-22 22:10:46 +0800

    added content to About page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Title from '../components/UI/Title';
 
 const About: React.FC = () => {
-    return <div>About</div>
+    return <>
+    <Title title={"About"} desc={"This web forum app was made for the CVWO assignment in 2022."}/>
+    </>
 }
 
 export default About;
