99dcc733b7b59e91ee80132e6169135ad74d3abf
raynertjx <rayner742@gmail.com>
2023-01-22 20:24:12 +0800

    added actions for crumbslice

--- src/routes/Subforum.tsx
+++ src/routes/Subforum.tsx
@@ -6,21 +6,26 @@ import SubforumContainer from "../components/subforum/SubforumContainer";
 import { useParams, useLocation } from "react-router-dom";
 import { threadServices } from "../services/Services";
 import { spliceForumId } from "../helpers/helpers";
+import { breadcrumbActions } from "../store/breadcrumb-slice";
 
 const Subforum: React.FC = () => {
     const location = useLocation();
     const { forumId } = useParams() as { forumId: string };
-    const forumCategoryId = spliceForumId(forumId); 
+    const forumCategoryId = spliceForumId(forumId);
     const { title, subtitle } = location.state;
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         dispatch(threadActions.removeAllThreads());
+        dispatch(breadcrumbActions.removeAllCrumbs());
+        dispatch(breadcrumbActions.getAllCrumbs(title));
         const fetchThreads = async () => {
-            await threadServices.get_threads_from_cat(forumCategoryId).then((res) => {
-                console.log(res.data);
-                dispatch(threadActions.getAllThreads(res.data));
-            });
+            await threadServices
+                .get_threads_from_cat(forumCategoryId)
+                .then((res) => {
+                    console.log(res.data);
+                    dispatch(threadActions.getAllThreads(res.data));
+                });
         };
         fetchThreads();
     }, []);
