0e9f6de6b9a387d4bb4f8a09d13b98911bb68cf9
raynertjx <rayner742@gmail.com>
2023-01-23 15:41:14 +0800

    modified renderComments to include commentNo

--- src/components/comments/CommentsList.tsx
+++ src/components/comments/CommentsList.tsx
@@ -3,10 +3,11 @@ import { useAppSelector } from "../../helpers/hooks";
 import CommentItem from "./CommentItem";
 
 const CommentsList: React.FC = () => {
-    const comments = useAppSelector((state) => state.comments).map(
-        (comment) => <CommentItem commentId={comment.id} commentUserId={comment.user_id} commentContent={comment.content} commentAuthor={comment.author} commentDate={comment.created_at}/>
+    const comments = useAppSelector((state) => state.comments)
+    const renderComments = comments.map(
+        (comment) => <CommentItem commentId={comment.id} commentUserId={comment.user_id} commentContent={comment.content} commentAuthor={comment.author} commentDate={comment.created_at} commentNo={comments.indexOf(comment) + 2}/>
     );
-    return <ul>{comments}</ul>;
+    return <ul>{renderComments}</ul>;
 };
 
 export default CommentsList;
