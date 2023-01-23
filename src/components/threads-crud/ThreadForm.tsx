74e98a5bf75cd2c65b1745a11ddccd0d79755dfb
raynertjx <rayner742@gmail.com>
2023-01-23 15:43:56 +0800

    added styling to ThreadForm

--- src/components/threads-crud/ThreadForm.tsx
+++ src/components/threads-crud/ThreadForm.tsx
@@ -7,34 +7,62 @@ import { threadActions } from "../../store/thread-slice";
 type Prop = {
     formSubmitHandler: (event: React.FormEvent) => {};
     titleRef: React.Ref<HTMLInputElement>;
-    contentRef: React.Ref<HTMLInputElement>;
+    contentRef: React.Ref<HTMLTextAreaElement>;
     titleValue: string;
     contentValue: string;
 };
 
 const ThreadForm: React.FC<Prop> = (props: Prop) => {
     return (
-        <form onSubmit={props.formSubmitHandler}>
-            <label htmlFor="thread_title">Thread Title</label>
-            <input
-                placeholder="Title"
-                type="text"
-                name="thread_title"
-                id="thread_title"
-                ref={props.titleRef}
-                defaultValue={props.titleValue}
-            />
-            <label htmlFor="thread_content">Content</label>
-            <input
-                placeholder="Content"
-                type="text"
-                name="thread_content"
-                id="thread_content"
-                ref={props.contentRef}
-                defaultValue={props.contentValue}
-            />
-            <input type="submit" value="Submit" />
-        </form>
+        <div className="flex justify-center">
+            <form
+                onSubmit={props.formSubmitHandler}
+                className="w-8/12 flex flex-col gap-2"
+            >
+                <div className="flex flex-col gap-1">
+                    <label
+                        htmlFor="thread_title"
+                        className="font-medium text-xl"
+                    >
+                        Title
+                    </label>
+                    <input
+                        placeholder="Title"
+                        type="text"
+                        name="thread_title"
+                        id="thread_title"
+                        ref={props.titleRef}
+                        defaultValue={props.titleValue}
+                        className="rounded border-2"
+                        maxLength={50}
+                    />
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label
+                        htmlFor="thread_content"
+                        className="font-medium text-xl"
+                    >
+                        Content
+                    </label>
+                    <textarea
+                        placeholder="Content"
+                        name="thread_content"
+                        id="thread_content"
+                        ref={props.contentRef}
+                        defaultValue={props.contentValue}
+                        className="rounded border-2 h-44"
+                        maxLength={450}
+                    />
+                </div>
+                <div className="flex justify-end">
+                    <input
+                        type="submit"
+                        value="Submit"
+                        className="bg-blue-400 rounded py-1 px-4 font-medium"
+                    />
+                </div>
+            </form>
+        </div>
     );
 };
 
