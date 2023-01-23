6bdd96b8f909fa071f8f2c6366eafad52ab68d85
raynertjx <rayner742@gmail.com>
2023-01-23 21:19:44 +0800

    sorted comments from oldest to newest

--- src/components/comments/CommentsList.tsx
+++ src/components/comments/CommentsList.tsx
@@ -3,10 +3,19 @@ import { useAppSelector } from "../../helpers/hooks";
 import CommentItem from "./CommentItem";
 
 const CommentsList: React.FC = () => {
-    const comments = useAppSelector((state) => state.comments)
-    const renderComments = comments.map(
-        (comment) => <CommentItem commentId={comment.id} commentUserId={comment.user_id} commentContent={comment.content} commentAuthor={comment.author} commentDate={comment.created_at} commentNo={comments.indexOf(comment) + 2}/>
-    );
+    const comments = useAppSelector((state) => state.comments);
+    const arrayForSort = [...comments];
+    const sortedComments = arrayForSort.sort((a, b) => a.created_at > b.created_at ? 1 : -1);
+    const renderComments = sortedComments.map((comment) => (
+        <CommentItem
+            commentId={comment.id}
+            commentUserId={comment.user_id}
+            commentContent={comment.content}
+            commentAuthor={comment.author}
+            commentDate={comment.created_at}
+            commentNo={comments.indexOf(comment) + 2}
+        />
+    ));
     return <ul>{renderComments}</ul>;
 };
 
