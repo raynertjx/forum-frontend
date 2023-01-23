a9cc9c9ffc592a0241956c02d9e06bc27cb83374
raynertjx <rayner742@gmail.com>
2023-01-23 15:42:30 +0800

    added styling to CommentForm

--- src/components/comments-crud/CommentForm.tsx
+++ src/components/comments-crud/CommentForm.tsx
@@ -2,24 +2,27 @@ import React, { useRef } from "react";
 
 type Prop = {
     formSubmitHandler: (event: React.FormEvent) => {};
-    contentRef: React.Ref<HTMLInputElement>;
+    contentRef: React.Ref<HTMLTextAreaElement>;
     contentValue: string;
 };
 
 const CommentForm: React.FC<Prop> = (props: Prop) => {
     return (
-        <form onSubmit={props.formSubmitHandler}>
-            <label htmlFor="comment_content">Comment</label>
-            <input
-                placeholder="Type your comment here!"
-                type="text"
-                name="comment_content"
-                id="comment_content"
-                ref={props.contentRef}
-                defaultValue={props.contentValue}
-            />
-            <input type="submit" value="Submit" />
-        </form>
+        <div>
+            <form onSubmit={props.formSubmitHandler} className="flex flex-col gap-2">
+                <label htmlFor="comment_content" className="font-medium pt-2 pl-2">New Comment</label>
+                <textarea
+                    placeholder="Type your comment here!"
+                    name="comment_content"
+                    id="comment_content"
+                    ref={props.contentRef}
+                    defaultValue={props.contentValue}
+                    className="h-40 p-4 rounded border-2"
+                    maxLength={450}
+                />
+                <div className="flex justify-end"><input type="submit" value="Submit" className="bg-blue-400 transition py-1 px-4 font-medium rounded hover:bg-blue-500" /></div>
+            </form>
+        </div>
     );
 };
 
